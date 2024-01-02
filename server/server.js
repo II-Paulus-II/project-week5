4725e7267556b470a1aa9d8cd041fcc10dcd059d
II-Paulus-II <paulus.coding@gmail.com>
2024-01-02 09:57:33 +0000

    added endpoint and test for endpoint

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Basic Endpoint
+app.get("/message", function (request, response) {
+  response.json({ message: "I am the message" });
+});
 
 app.listen(8080, () => {
   console.log("Server is Running");
