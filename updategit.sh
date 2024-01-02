9260a33db08503c4d00ca520b2410bda6df14634
II-Paulus-II <paulus.coding@gmail.com>
2024-01-02 22:10:55 +0000

    added script for thomas and kayleigh to make project & updated updategit.sh

--- updategit.sh
+++ updategit.sh
@@ -8,3 +8,9 @@ git push origin paulus
 git switch eric
 git merge main
 git push origin eric
+git switch kayleigh
+git merge main
+git push origin kayleigh
+git switch thomas
+git merge main
+git push origin thomas
