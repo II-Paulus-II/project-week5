8f0504eb5d0e7aa036d80b6d40b14481836d2301
Eric Dardamissis <ironeric5@outlook.com>
2024-01-02 15:07:45 +0000

    basic memory game shell

--- client/app.js
+++ client/app.js
@@ -1,1 +1,41 @@
 console.log("Script Loaded");
+
+
+let furnitureArray = [
+    {name: "Chair"},
+    {name: "Table"},
+    {name: "Sofa"},
+    {name: "Bed"},
+    {name: "Desk"},
+    {name: "Cabinet"},
+    {name: "Dresser"},
+    {name: "Bookcase"},
+    {name: "Recliner"},
+    {name: "Ottoman"}
+];
+
+
+let gameScreen = document.getElementById('gameScreen');
+let startButton = document.getElementById('startButton');
+
+startButton.addEventListener('click', function() {
+  // Insert furniture names into the div
+  furnitureArray.forEach(function(furniture) {
+    let furnitureDiv = document.createElement('div');
+    furnitureDiv.textContent = furniture.name;
+    console.log(furniture.name)
+    gameScreen.appendChild(furnitureDiv);
+  });
+
+  // Make the game screen visible
+  gameScreen.style.opacity = 1;
+
+  // Make the furniture names disappear after 15 seconds
+  setTimeout(function() {
+    while (gameScreen.firstChild) {
+      gameScreen.removeChild(gameScreen.firstChild);
+    }
+    gameScreen.style.opacity = 1;
+  }, 1500); 
+
+});
