72e6d8b32dd3f31e98379413aa5a68336613b43b
II-Paulus-II <paulus.coding@gmail.com>
2024-01-02 15:35:24 +0000

    basic server side get request

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
+/* ----- Imports ----- */
+
 import express from "express";
 import cors from "cors";
+import Database from "better-sqlite3";
 
+/* ----- Global Variables ----- */
+
+const db = new Database("database.db");
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Basic Endpoint
+/* ----- Endpoints ----- */ 
+
 app.get("/message", function (request, response) {
   response.json({ message: "newmessage" });
+});
+
+app.get("/getmemory", function (request, response) {
+  const memory = db.prepare(`SELECT * FROM items`).all();
+  response.json(memory);
 });
 
 app.listen(8080, () => {
--- server/testendpoints.sh
+++ server/testendpoints.sh
@@ -2,3 +2,5 @@
 
 curl http://localhost:8080/message
 echo -e
+curl http://localhost:8080/getmemory
+echo -e
