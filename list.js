1e492a8b869cc42b3933f7a265e0900bbe6d273f
II-Paulus-II <paulus.coding@gmail.com>
2024-01-03 09:30:58 +0000

    list commands js file

---
+++ list.js
@@ -0,0 +1,25 @@
+/* ---- Lists of Commands ----- */ 
+
+console.log(" ----- SCRIPTS ----- ");
+console.log("")
+console.log("Main Folder:");
+console.log("npm run updategit");
+console.log("### After main branch on github is fully updated - updates all local branches.");
+console.log("npm run list");
+console.log('### Lists all "npm run" commands');
+console.log("");
+console.log("Client Folder:");
+console.log("npm run dev");
+console.log("### Hosts the front end of the application.");
+console.log("npm run build");
+console.log("### Builds and optimises front end for hosting.");
+console.log("");
+console.log("Server Folder:");
+console.log("npm run start");
+console.log("### Runs a local copy of the backend server.");
+console.log("npm run test");
+console.log("### Tests the backend server for response for each endpoint.");
+console.log("npm run setup");
+console.log("### Makes the database for the first time.");
+console.log("npm run clean");
+console.log("### Completely remakes the database from seed.js");
